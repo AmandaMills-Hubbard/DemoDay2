37e8431667a551b704b92ac7533361bf43c5ff9a
Amanda Mills-Hubbard <amanda.millshubbard@gmail.com>
2020-04-04 14:08:04 -0400

    Add files via upload
    Some glitches

---
+++ DemoDay/public/side.js
@@ -0,0 +1,47 @@
+
+var vote = document.getElementsByClassName("fa-check");
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(vote).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const vote = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('comments', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'vote':vote
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('comments', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
